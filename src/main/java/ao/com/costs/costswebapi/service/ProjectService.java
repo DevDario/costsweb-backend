cd8f4e45ed737ac1b2fc1940c206ec3014b588a0
Dário Silva <dariosilva13222@gmail.com>
2024-09-20 16:17:31 +0100

    feat: returns all projects from db

--- src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
@@ -1,8 +1,21 @@
 package ao.com.costs.costswebapi.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import ao.com.costs.costswebapi.domain.Project;
+import ao.com.costs.costswebapi.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
     
+    @Autowired
+    ProjectRepository projectRepository;
+
+    // GET's all projects
+    public List<Project> getAllProjects(){
+        return projectRepository.findAll();
+    }
 }
