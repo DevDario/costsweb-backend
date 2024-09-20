0a26061f6e9ac6b8b482d346c41ed05c8e896cc0
Dário Silva <dariosilva13222@gmail.com>
2024-09-20 12:10:48 +0100

    inits springboot project

---
+++ src/main/java/ao/com/costs/costswebapi/CostswebapiApplication.java
@@ -0,0 +1,13 @@
+package ao.com.costs.costswebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostswebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostswebapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/com/costs/costswebapi/CostswebapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.com.costs.costswebapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostswebapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
