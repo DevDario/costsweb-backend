d089a0ea980e6249b0a0fbf136b7aa0f022d19fc
Dário Silva <dariosilva13222@gmail.com>
2024-09-20 15:11:14 +0100

    fix: adds a reference to the 'project' table

--- src/main/resources/db/migrations/V1__create_service_table.sql
+++ src/main/resources/db/migrations/V1__create_service_table.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS costsweb.service(
     id INT NOT NULL AUTO_INCREMENT,
+    projectID INT,
     name VARCHAR(255) NOT NULL,
     budget DECIMAL(5,3) NOT NULL,
     description VARCHAR(255) DEFAULT 'none',
     numberOfServices INT NOT NULL DEFAULT 0,
-    CONSTRAINT PRIMARY KEY(id)
+    CONSTRAINT PRIMARY KEY(id),
+    FOREIGN KEY(projectID) REFERENCES project(id)
 );
