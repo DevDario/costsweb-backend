74c008c84151b49fc742f169813c378b84635023
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 15:56:22 +0100

    feat(raw): new endpoint for new project submit

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,12 @@ public class ProjectController {
     
     @Autowired
     ProjectService projectService;
+
+    @PostMapping("project/new")
+    public ResponseEntity<?> createProject(@RequestBody Project project) {
+        return projectService.createProject(project);
+    }
+    
 
     @GetMapping("/project/all")
     public List<Project> getAllProjects(){
