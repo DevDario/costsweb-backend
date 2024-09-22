cf6826fe272f72c1b9a8999433cc29b8bac1e500
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 15:46:13 +0100

    feat(raw): creates new endpoint for project deletion

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,5 +36,10 @@ public class ProjectController {
     @PutMapping("project/edit/{id}")
     public ResponseEntity<?> editProject(@PathVariable Long id, @RequestBody Project projectDetails) throws ProjectNotFoundException {
         return projectService.editProject(id, projectDetails);
+    }
+
+    @DeleteMapping("project/del/{id}")
+    public ResponseEntity<?> deleteProject(@PathVariable Long id) throws ProjectNotFoundException{
+        return projectService.deleteProject(id);
     }
 }
