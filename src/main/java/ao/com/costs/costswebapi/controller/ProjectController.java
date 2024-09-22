a2bb6bf4e8b59b6e91ca7cbdeaaedc977ad7304b
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 12:37:04 +0100

    raw: creates endpoint to get a single project

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ao.com.costs.costswebapi.domain.Project;
+import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
 import ao.com.costs.costswebapi.service.ProjectService;
 
 
@@ -20,5 +22,9 @@ public class ProjectController {
     public List<Project> getAllProjects(){
         return projectService.getAllProjects();
     }
-    
+
+    @GetMapping("project/{id}")
+    public Project getProject(@PathVariable Long id) throws ProjectNotFoundException{
+        return projectService.getSingleProject(id);
+    }
 }
