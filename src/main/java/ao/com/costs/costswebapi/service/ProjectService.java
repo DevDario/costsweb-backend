200e5153f568e8497e2e3028a56f8c9fbf052ba2
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 15:29:22 +0100

    feat: edits a project
    raw: edits a project, getting the new details from the request body

--- src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
@@ -3,7 +3,10 @@ package ao.com.costs.costswebapi.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import ao.com.costs.costswebapi.domain.Project;
 import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
@@ -23,5 +26,24 @@ public class ProjectService {
     // GET one project
     public Project getSingleProject(Long id) throws ProjectNotFoundException{
         return projectRepository.findById(id).orElseThrow(() -> new ProjectNotFoundException(id));
+    }
+
+    // EDIT's(PUT) a project
+    public ResponseEntity<?> editProject(@PathVariable Long id, @RequestBody Project projectDetails) throws ProjectNotFoundException{
+        
+        Project project = projectRepository.findById(id).orElseThrow(()-> new ProjectNotFoundException(id));
+
+        project.setName(projectDetails.getName());
+        project.setBudget(projectDetails.getBudget());
+        project.setCategory(projectDetails.getCategory());
+        project.setNumServices(projectDetails.getNumServices());
+        project.setServiceID(projectDetails.getServiceID());
+        project.setUsedBudget(projectDetails.getUsedBudget());
+        project.setDeadline(projectDetails.getDeadline());
+        project.setCreatedAt(projectDetails.getCreatedAt());
+
+        projectRepository.save(project);
+
+        return ResponseEntity.ok().build();
     }
 }
