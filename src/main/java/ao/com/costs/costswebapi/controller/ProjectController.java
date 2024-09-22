f5b250ba0125631ae891c23ecb2aa6b29bc44dd6
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 15:30:05 +0100

    feat(raw): creates endpoint to edit a project

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectController.java
@@ -3,13 +3,17 @@ package ao.com.costs.costswebapi.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import ao.com.costs.costswebapi.domain.Project;
 import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
 import ao.com.costs.costswebapi.service.ProjectService;
+
 
 
 @RestController
@@ -26,5 +30,10 @@ public class ProjectController {
     @GetMapping("project/{id}")
     public Project getProject(@PathVariable Long id) throws ProjectNotFoundException{
         return projectService.getSingleProject(id);
+    }
+
+    @PutMapping("project/edit/{id}")
+    public ResponseEntity<?> editProject(@PathVariable Long id, @RequestBody Project projectDetails) throws ProjectNotFoundException {
+        return projectService.editProject(id, projectDetails);
     }
 }
