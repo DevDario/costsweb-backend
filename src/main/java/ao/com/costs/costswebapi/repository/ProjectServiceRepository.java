4f8ed254b71d7b7bbfff7d4d7e3837c59e115a07
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 17:15:44 +0100

    feat: creates Service repository interface

---
+++ src/main/java/ao/com/costs/costswebapi/repository/ProjectServiceRepository.java
@@ -0,0 +1,10 @@
+package ao.com.costs.costswebapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ao.com.costs.costswebapi.domain.Service;
+
+
+public interface ProjectServiceRepository extends JpaRepository<Service, Long> {
+
+}
