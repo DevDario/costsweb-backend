8b2671496f3b58dba711bf2b4f1a5ec8b8124ca9
Dário Silva <dariosilva13222@gmail.com>
2024-09-22 12:29:19 +0100

    feat: find's project by id, or else throws custom exception

--- src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ao.com.costs.costswebapi.domain.Project;
+import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
 import ao.com.costs.costswebapi.repository.ProjectRepository;
 
 @Service
@@ -17,5 +18,10 @@ public class ProjectService {
     // GET's all projects
     public List<Project> getAllProjects(){
         return projectRepository.findAll();
+    }
+
+    // GET one project
+    public Project getSingleProject(Long id) throws ProjectNotFoundException{
+        return projectRepository.findById(id).orElseThrow(() -> new ProjectNotFoundException(id));
     }
 }
