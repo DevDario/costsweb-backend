e6aa124754f281ec97efb1043280e21c2166dd00
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 11:36:12 +0100

    feat: returns all services from all projects

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
@@ -1,7 +1,10 @@
 package ao.com.costs.costswebapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ao.com.costs.costswebapi.domain.ProjectService;
 import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
+import ao.com.costs.costswebapi.repository.ProjectServiceRepository;
 import ao.com.costs.costswebapi.service.ProjectServiceService;
 
 @RestController
@@ -17,10 +21,22 @@ public class ProjectServiceController {
     @Autowired
     ProjectServiceService projectServiceService;
 
+    @Autowired
+    ProjectServiceRepository projectServiceRepository;
 
+
+    // CREATE's a new service 'inside' a project
     @PostMapping("/project/{projectid}/services/new")
     public ResponseEntity<?> addNewService(@PathVariable Long projectid, @RequestBody ProjectService newService) throws ProjectNotFoundException,Exception{
         return projectServiceService.addNewService(projectid, newService) ;
     }
+
+    
+    // GET's all services from all projects
+    @GetMapping("/service/all")
+    public List<ProjectService> getAllServices() {
+        return projectServiceRepository.findAll();
+    }
+    
 
 }
