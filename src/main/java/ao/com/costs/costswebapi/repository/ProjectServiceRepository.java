6527fa7f286cccaadbf4d40582140ad4aacf0f5d
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 11:56:02 +0100

    feat: find services by project id

--- src/main/java/ao/com/costs/costswebapi/repository/ProjectServiceRepository.java
+++ src/main/java/ao/com/costs/costswebapi/repository/ProjectServiceRepository.java
@@ -1,5 +1,7 @@
 package ao.com.costs.costswebapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ao.com.costs.costswebapi.domain.ProjectService;
@@ -7,4 +9,5 @@ import ao.com.costs.costswebapi.domain.ProjectService;
 
 public interface ProjectServiceRepository extends JpaRepository<ProjectService, Long> {
 
+    List<ProjectService> findByProjectid(Long projectid);
 }
