b133504644d51d03a3bc3c19c7d367b66f7d004b
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 09:29:12 +0100

    feat: creates method to add a new service to given project

--- src/main/java/ao/com/costs/costswebapi/service/ProjectServiceService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectServiceService.java
@@ -1,6 +1,33 @@
 package ao.com.costs.costswebapi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import ao.com.costs.costswebapi.domain.ProjectService;
+import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
+import ao.com.costs.costswebapi.repository.ProjectRepository;
+import ao.com.costs.costswebapi.repository.ProjectServiceRepository;
+
+@Service
 public class ProjectServiceService {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Autowired
+    ProjectServiceRepository projectServiceRepository;
     
+    // ADD's a new service to a project
+    public ResponseEntity<?> addNewService(Long projectID, @RequestBody ProjectService newService) throws ProjectNotFoundException{
+
+        projectRepository.findById(projectID).orElseThrow(()-> new ProjectNotFoundException(projectID));
+
+        projectServiceRepository.save(newService);
+
+        return ResponseEntity.ok().build();
+
+    }
+
 }
