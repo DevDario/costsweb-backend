172924d8458b285dde31307320027cb64625ab50
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 12:37:31 +0100

    raw(feat): new endpoint for deleting a service

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +42,10 @@ public class ProjectServiceController {
         return projectServiceService.getServicesFromProject(projectid);
     }
     
-    
+    // DELETE's a service
+    @DeleteMapping("/project/{projectid}/service/services/{serviceid}")
+    public ResponseEntity<?> deleteService(@PathVariable Long projectid, @PathVariable Long serviceid) throws ProjectNotFoundException,Exception{
+        return projectServiceService.deleteService(projectid, serviceid);
+    }
 
 }
