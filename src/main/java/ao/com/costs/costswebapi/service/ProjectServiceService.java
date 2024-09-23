8e893ddec67835513122e69637a98a30905ec79a
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 11:33:13 +0100

    feat(major): adds a new service to a project
    raw: returns custom ResponseEntity response
    
    raw: validates service budget
    
    raw: edits projects budget, usedbudget and number of services properties as the service is added
    
    refactor: changes project id's property name

--- src/main/java/ao/com/costs/costswebapi/service/ProjectServiceService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectServiceService.java
@@ -1,11 +1,13 @@
 package ao.com.costs.costswebapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import ao.com.costs.costswebapi.domain.Project;
 import ao.com.costs.costswebapi.domain.ProjectService;
 import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
 import ao.com.costs.costswebapi.repository.ProjectRepository;
@@ -21,13 +23,27 @@ public class ProjectServiceService {
     ProjectServiceRepository projectServiceRepository;
     
     // ADD's a new service to a project
-    public ResponseEntity<?> addNewService(@PathVariable Long projectID, @RequestBody ProjectService newService) throws ProjectNotFoundException{
+    public ResponseEntity<?> addNewService(@PathVariable Long projectid, @RequestBody ProjectService newService) throws ProjectNotFoundException,Exception{
 
-        projectRepository.findById(projectID).orElseThrow(()-> new ProjectNotFoundException(projectID));
+        Project project = projectRepository.findById(projectid).orElseThrow(()-> new ProjectNotFoundException(projectid));
+
+        if(newService.getBudget() > project.getBudget()){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("There's not enough budget for this service");
+        }
 
         projectServiceRepository.save(newService);
 
-        return ResponseEntity.ok().build();
+        project.setNumServices(project.getNumServices() + 1);
+        project.setBudget(project.getBudget() - newService.getBudget());
+        project.setUsedBudget(project.getUsedBudget() + newService.getBudget());
+
+        projectRepository.save(project);
+
+        return ResponseEntity
+                .ok()
+                .body(newService.getName() + " service added !");
 
     }
 
