0a539dd053f7fc3dee07fcb40626f513d40cc6c8
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 09:31:37 +0100

    fix: decorates class with @RestController

--- src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
+++ src/main/java/ao/com/costs/costswebapi/controller/ProjectServiceController.java
@@ -5,11 +5,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import ao.com.costs.costswebapi.domain.ProjectService;
 import ao.com.costs.costswebapi.exception.ProjectNotFoundException;
 import ao.com.costs.costswebapi.service.ProjectServiceService;
 
+@RestController
 public class ProjectServiceController {
     
     @Autowired
