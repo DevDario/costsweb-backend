90efb82e27570b4e4ec83bef52c0ca9446a70921
Dário Silva <dariosilva13222@gmail.com>
2024-09-23 09:03:18 +0100

    refactor: changes class name

---
+++ src/main/java/ao/com/costs/costswebapi/domain/ProjectService.java
@@ -0,0 +1,40 @@
+package ao.com.costs.costswebapi.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name="service")
+public class ProjectService {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="id")
+    private Long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="budget")
+    private float budget;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="projectID")
+    private Long projectID;
+
+}
