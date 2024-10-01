25bcff8b5d5e16346aa11fecf292560634e2fbd9
Dário Silva <dariosilva13222@gmail.com>
2024-10-01 23:49:59 +0100

    fix: adds a reference to 'Project' Entity
    raw: specifies the relationship between both Entity's

--- src/main/java/ao/com/costs/costswebapi/domain/ProjectService.java
+++ src/main/java/ao/com/costs/costswebapi/domain/ProjectService.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -36,5 +38,9 @@ public class ProjectService {
 
     @Column(name="projectid")
     private Long projectid;
+
+    @ManyToOne
+    @JoinColumn(name="projectid")
+    private Project project;
 
 }
