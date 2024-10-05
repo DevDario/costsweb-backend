0a341bfb152af67c48a80521343fa6b7c0d9fa9b
Dário Silva <dariosilva13222@gmail.com>
2024-10-05 10:39:17 +0100

    refactor: removes 'column' decorator from 'services'
    raw: because this field is/ represents a relationship
    
    refactor: removes unnecessary 'numservices' field
    
    feat: added cascading and orphanRemoval behaviors to 'services' field

--- src/main/java/ao/com/costs/costswebapi/domain/Project.java
+++ src/main/java/ao/com/costs/costswebapi/domain/Project.java
@@ -4,16 +4,8 @@ import java.util.Date;
 import java.util.List;
 
 import ao.com.costs.costswebapi.enums.Category;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -33,30 +25,26 @@ public class Project {
     @Column(name="id")
     private Long id;
 
-    @Column(name="name")
+    @Column(name="name", nullable = false)
     private String name;
 
-    @Column(name="budget")
+    @Column(name="budget", nullable = false)
     private float budget;
 
-    @Column(name="usedBudget")
+    @Column(name="usedBudget", nullable = false)
     private float usedBudget;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="category")
+    @Column(name="category", nullable = false)
     private Category category;
 
-    @Column(name="numServices")
-    private Long numServices;
-
-    @Column(name="createdAt")
+    @Column(name="createdAt", nullable = false)
     private Date createdAt;
 
     @Column(name="deadline")
     private Date deadline;
 
-    @JoinColumn(name="projectid")
-    @OneToMany(mappedBy="project")
-    @Column(name="services")
+    @OneToMany(mappedBy="project", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference
     private List<ProjectService> services;
 }
