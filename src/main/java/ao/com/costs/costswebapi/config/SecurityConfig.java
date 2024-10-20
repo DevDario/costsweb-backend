fdaeeb43d5ad91ec9dea67ed2ee42a86c2791477
Dário Silva <dariosilva13222@gmail.com>
2024-10-20 14:11:13 +0100

    feat: creates security configuration file

---
+++ src/main/java/ao/com/costs/costswebapi/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package ao.com.costs.costswebapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
