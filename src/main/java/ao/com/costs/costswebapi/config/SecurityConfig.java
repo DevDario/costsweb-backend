ac637ee28dd71f804551b74f3a86ebce0119f196
Dário Silva <dariosilva13222@gmail.com>
2024-10-21 10:00:52 +0100

    feat: sets authorization protocols and routes in securityFilterChaain bean

--- src/main/java/ao/com/costs/costswebapi/config/SecurityConfig.java
+++ src/main/java/ao/com/costs/costswebapi/config/SecurityConfig.java
@@ -1,9 +1,41 @@
 package ao.com.costs.costswebapi.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/Auth/login","/login","/css/**","/js/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/home",true)
+                        .failureUrl("/login?error=true")
+                        .permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/Auth/login")
+                        .defaultSuccessUrl("/home",true)
+                        .failureUrl("/Auth/login?error=true")
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll()
+                );
+
+
+        return http.build();
+    }
 }
