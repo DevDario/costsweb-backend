23f95bc63a9d11e0402cda7dc1b37cec2bfeda91
Dário Silva <dariosilva13222@gmail.com>
2024-10-23 14:25:20 +0100

    feat: sets session policy to STATELESS
    fix: leaves only oauth2.0 login

--- src/main/java/ao/com/costs/costswebapi/config/SecurityConfig.java
+++ src/main/java/ao/com/costs/costswebapi/config/SecurityConfig.java
@@ -2,8 +2,11 @@ package ao.com.costs.costswebapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,28 +16,13 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http.csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**","/login","/css/**","/js/**").permitAll()
                                 .anyRequest().authenticated()
                 )
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .loginProcessingUrl("/login")
-                        .defaultSuccessUrl("/home",true)
-                        .failureUrl("/login?error=true")
-                        .permitAll()
-                )
-                .oauth2Login(oauth2 -> oauth2
-                        .loginPage("/Auth/login")
-                        .defaultSuccessUrl("/home",true)
-                        .failureUrl("/Auth/login?error=true")
-                )
-                .logout(logout -> logout
-                        .logoutUrl("/logout")
-                        .logoutSuccessUrl("/login?logout=true")
-                        .permitAll()
-                );
-
+                .oauth2Login(oauth -> oauth.permitAll())
+                .oauth2Login(Customizer.withDefaults());
 
         return http.build();
     }
