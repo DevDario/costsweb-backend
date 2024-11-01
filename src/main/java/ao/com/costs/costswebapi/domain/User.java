63d9cd05ac1470f33e1012ca33ef92ed1cd20ab0
Dário Silva <dariosilva13222@gmail.com>
2024-11-01 13:33:29 +0100

    fix: sets user entity email property as UNIQUE

--- src/main/java/ao/com/costs/costswebapi/domain/User.java
+++ src/main/java/ao/com/costs/costswebapi/domain/User.java
@@ -24,7 +24,7 @@ public class User {
     @Column(name = "name", nullable = false)
     private String name;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @Column(name = "github_user_name")
