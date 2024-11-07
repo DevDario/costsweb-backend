3ed2af14b846b0f1e9dc9546ead90928252e853e
Dário Silva <dariosilva13222@gmail.com>
2024-11-07 16:46:24 +0100

    feat: maps an endpoint to fetch user data based on the user email fetched from the Spring Security's Authentication Object

---
+++ src/main/java/ao/com/costs/costswebapi/controller/UserController.java
@@ -0,0 +1,35 @@
+package ao.com.costs.costswebapi.controller;
+
+import ao.com.costs.costswebapi.domain.User;
+import ao.com.costs.costswebapi.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/user/")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<User> getUserDetails(Authentication auth) throws Exception{
+        if(!(auth == null)){
+            OAuth2User user = (OAuth2User) auth.getPrincipal();
+            String email = user.getAttribute("email");
+
+            User userDetails = this.userService.getUserByEmail(email);
+            return ResponseEntity.ok().body(userDetails);
+
+        }else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+}
