4774e311cd24da022960a2de9148d527837b2253
Dário Silva <dariosilva13222@gmail.com>
2024-11-10 21:36:05 +0100

    feat: creates user-focused custom methods

--- src/main/java/ao/com/costs/costswebapi/repository/ProjectRepository.java
+++ src/main/java/ao/com/costs/costswebapi/repository/ProjectRepository.java
@@ -1,9 +1,18 @@
 package ao.com.costs.costswebapi.repository;
 
+import ao.com.costs.costswebapi.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ao.com.costs.costswebapi.domain.Project;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    
+
+    List<Project> findByUser(User user);
+
+    Optional<Project> findByIdAndUser(Long id, User user);
+
+    void deleteByIdAndUser(Long id, User user);
 }
