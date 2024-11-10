0078856e32ce69dd0649d7b7e104546a1cd3af98
Dário Silva <dariosilva13222@gmail.com>
2024-11-10 05:42:10 +0100

    refactor: removes unused import

--- src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
+++ src/main/java/ao/com/costs/costswebapi/service/ProjectService.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import ao.com.costs.costswebapi.domain.User;
-import ao.com.costs.costswebapi.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
